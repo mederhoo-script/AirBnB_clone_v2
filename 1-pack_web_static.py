c904dc78a68b1d08b79e289ee75f5006adc3f98a
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-02-04 23:55:16 +0100

    farbric set tar

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-# Fabfile to generates a .tgz archive from the contents of web_static.
-import os.path
+# Fabric script that generates a .tgz archive from the contents of the web_static folder
+# of your AirBnB Clone repo, using the function do_pack.import os.path
 from datetime import datetime
 from fabric.api import local
 
 
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
+    """Creating a tar gzipped archive of the directory web_static."""
+    d_t = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(d_t.year,
+                                                         d_t.month,
+                                                         d_t.day,
+                                                         d_t.hour,
+                                                         d_t.minute,
+                                                         d_t.second)
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
