f21f8e3562a223e0cf2a8b1d491f53eeccf774b8
hammed amusat oriyomi <mederhoo@gmail.com>
2024-02-04 13:36:00 -0800

    airbnb nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # Install Nginx if not already installed
-apt-get update
-apt-get -y install nginx
+sudo apt-get update
+sudo apt-get -y install nginx
 
 # Create necessary directories if they don't exist
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
 
 # Create a fake HTML file for testing
 echo "<html>
@@ -14,16 +14,16 @@ echo "<html>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Create symbolic link and give ownership of /data/ to ubuntu user and group
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
 
 # Update Nginx configuration to serve content of /data/web_static/current/ to hbnb_static
 config="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-sed -i "/server_name _;/a $config" /etc/nginx/sites-available/default
+sudo sed -i "/server_name _;/a $config" /etc/nginx/sites-available/default
 
 # Restart Nginx to apply changes
-service nginx restart
+sudo service nginx restart
