9e56cc96a64b9b7423ac97f6dcdb3e3d2c77c304
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-02-04 23:16:04 +0100

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,8 +22,27 @@ sudo chown -R ubuntu /data/
 sudo chgrp -R ubuntu /data/
 
 # Update Nginx configuration to serve content of /data/web_static/current/ to hbnb_static
-config="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-sudo sed -i "/server_name _;/a $config" /etc/nginx/sites-available/default
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" | sudo tee /etc/nginx/sites-available/default
 # Restart Nginx to apply changes
 sudo service nginx restart
